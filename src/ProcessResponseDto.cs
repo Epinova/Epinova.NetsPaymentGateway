721b811b711545871b0e9281fe75e5f31b6d3c01
Tarjei Olsen <tarjei.olsen@epinova.no>
2019-04-05 15:49:33 +0200

    Update infrastructure package

--- src/ProcessResponseDto.cs
+++ src/ProcessResponseDto.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Xml.Serialization;
-using Epinova.Infrastructure;
 
 namespace Epinova.NetsPaymentGateway
 {
     /// <remarks>DTO has to be public to enable XmlSerializer's deserialize support.</remarks>
     [XmlRoot("ProcessResponse")]
-    public class ProcessResponseDto : ServiceResponseBase
+    public class ProcessResponseDto : ResponseDtoBase
     {
         public string AuthorizationId { get; set; }
         public DateTimeOffset ExecutionTime { get; set; }
--- src/RegisterResponseDto.cs
+++ src/RegisterResponseDto.cs
@@ -1,11 +1,10 @@
 ﻿using System.Xml.Serialization;
-using Epinova.Infrastructure;
 
 namespace Epinova.NetsPaymentGateway
 {
     /// <remarks>DTO has to be public to enable XmlSerializer's deserialize support.</remarks>
     [XmlRoot("RegisterResponse")]
-    public class RegisterResponseDto : ServiceResponseBase
+    public class RegisterResponseDto : ResponseDtoBase
     {
         public string TransactionId { get; set; }
     }
